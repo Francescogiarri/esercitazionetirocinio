9ea963dda8b58c69f737e79dd1f6603cba69badb
Francescogiarri <francescogiarri@gmail.com>
2021-04-16 14:21:06 +0200

    aggiuta cartella

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    printf("hello world!\n");
+    int a,b;
+    double var;
+    a=1;
+    b=5;
+    var=a+b;
+    printf("a=%d,b=%d,var=%.0f\n",a,b,var);
+    var=b-a;
+    printf("a=%d,b=%d,var=%.0f\n",a,b,var);
+    return 0;
+
+}
+
+/*   stdio.h operazioni base per gestire input e output
+     math.h operaziani matematiche di base
+     main è la funzione da cui parte tutto il programma
+     printf = stampa su console
+ */
